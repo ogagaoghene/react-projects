7211d77bd315ade7a0625452a1df50131e42f71c
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2023-06-11 23:36:31 +0100

    Update index.js file

--- index.js
+++ index.js
@@ -4,6 +4,4 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('output'));
-
-// const mountNode = document.getElementById('output');
 root.render(<App />);
