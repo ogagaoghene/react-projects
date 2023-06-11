22c6367063380cd8ac3b53b77ab21982f66a645d
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2023-06-11 22:19:02 +0100

    Update App.js file

--- App.js
+++ App.js
@@ -2,17 +2,40 @@ import React from 'react';
 
 import './style.css';
 
-const App = props => {
-    const [displayName, setDisplayName] = useState();
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            displayName: '',
+            name: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick  = this.handleClick.bind(this);
+    }
 
-    return (
-        <>
-            <Title title="Welcome to the app"/>
-            <Greeting name={displayName}/>
-            <p>Enter your name below so we can get better acquainted</p>
-            <Input handleClick={name => setDisplayName(name)} />
-        </>
-    );
+    handleChange(event) {
+        this.setState({
+            name: event.target.value
+        })
+    };
+
+    handleClick(event) {
+        this.setState({
+            displayName: this.state.name
+        })
+    };
+
+    render() {
+        return (
+            <>
+                <h1>Welcome to the app</h1>
+                <p>Hi, there {this.state.displayName}</p>
+                <p>Enter your name below so we can get better acquainted</p>
+                <input value={this.state.name} onChange={this.handleChange} />
+                <button onClick={this.handleClick}>Update me</button>
+            </>
+        );
+    }
 }
 
 export default App;
