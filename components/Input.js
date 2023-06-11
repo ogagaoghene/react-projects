5e9f478b5ff08eaa26584b13dea185bab8acc0c8
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2023-06-11 20:35:16 +0100

    Update Input.js file

--- components/Input.js
+++ components/Input.js
@@ -1,2 +1,10 @@
 import React from 'react';
 
+const Input = props => {
+    return (
+        <>
+            <input type="text" value={this.state.name} onChange={this.handleChange}/>
+            <button onClick={this.handleClick}>Update name</button>
+        </>
+    )
+}
